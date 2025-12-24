51d02b598640a07cba1c9828644a8885f8b08850
mashulik <125238211+FursovaMashaa@users.noreply.github.com>
2025-12-24 18:29:34 +0400

    Update RentalPayloadDeserializer.cs

--- BikeRental/BikeRental.Infrastructure.Nats/Deserializers/RentalPayloadDeserializer.cs
+++ BikeRental/BikeRental.Infrastructure.Nats/Deserializers/RentalPayloadDeserializer.cs
@@ -7,14 +7,21 @@ namespace BikeRental.Infrastructure.Nats.Deserializers;
 
 /// <summary>
 /// NATS message deserializer for rental data batches.
+/// Implements <see cref="INatsDeserialize{T}"/> for converting byte sequences
+/// to lists of <see cref="RentalCreateUpdateDto"/> objects.
 /// </summary>
 public class BikeRentalPayloadDeserializer : INatsDeserialize<IList<RentalCreateUpdateDto>>
 {
-    /// <summary>
-    /// Deserializes a byte buffer to a list of rental DTOs.
+   /// <summary>
+    /// Deserializes a byte sequence from NATS message into a list of rental DTOs.
+    /// Uses <see cref="Utf8JsonReader"/> for efficient JSON parsing without
+    /// intermediate byte array allocation.
     /// </summary>
-    /// <param name="buffer">The byte sequence containing JSON data.</param>
-    /// <returns>Deserialized list of rental DTOs, or null if deserialization fails.</returns>
+    /// <param name="buffer">The byte sequence containing JSON-serialized rental data.</param>
+    /// <returns>
+    /// Deserialized list of <see cref="RentalCreateUpdateDto"/> objects,
+    /// or null if deserialization fails due to invalid JSON format.
+    /// </returns>
     public IList<RentalCreateUpdateDto>? Deserialize(in ReadOnlySequence<byte> buffer)
     {
         var reader = new Utf8JsonReader(buffer, isFinalBlock: true, state: default);
